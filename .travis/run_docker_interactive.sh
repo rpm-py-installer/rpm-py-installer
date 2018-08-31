d1a65b223b95fc33f206f843aaed3f4b2822ce5e
Jun Aruga <junaruga@users.noreply.github.com>
2018-08-31 15:38:03 +0200

    Update os versions for container tool. (#162)

--- .travis/run_docker_interactive.sh
+++ .travis/run_docker_interactive.sh
@@ -3,8 +3,9 @@
 set -ex
 
 # fedora image needs "bash".
+# CONTAINER_IMAGE=fedora:28
+# CONTAINER_IMAGE=fedora:27
 # CONTAINER_IMAGE=fedora:26
-# CONTAINER_IMAGE=fedora:25
 # CONTAINER_IMAGE=fedora:rawhide
 # CONTAINER_IMAGE=centos:7
 # CONTAINER_IMAGE=centos:6
