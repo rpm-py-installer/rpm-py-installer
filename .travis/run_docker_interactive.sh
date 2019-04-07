12f45feb0ba533dec8d0d16ef1e9b7fb8cfbd4ed
Jun Aruga <jaruga@redhat.com>
2019-04-07 16:29:22 +0200

    Remove possible harmful commands [ci skip]

--- .travis/run_docker_interactive.sh
+++ .travis/run_docker_interactive.sh
@@ -2,7 +2,6 @@
 
 set -ex
 
-# fedora image needs "bash".
 # CONTAINER_IMAGE=fedora:28
 # CONTAINER_IMAGE=fedora:27
 # CONTAINER_IMAGE=fedora:26
@@ -11,8 +10,5 @@ set -ex
 # CONTAINER_IMAGE=centos:6
 # CONTAINER_IMAGE=ubuntu:trusty
 CONTAINER_IMAGE=ubuntu:bionic
-
-docker pull "docker.io/${CONTAINER_IMAGE}"
-docker tag "docker.io/${CONTAINER_IMAGE}" "${CONTAINER_IMAGE}"
 
 docker run -it --rm "${CONTAINER_IMAGE}" bash
