0c3deef5a2edfeb14d23066f7a8db434ac54ec4e
Nikola Forró <nforro@redhat.com>
2023-05-10 15:59:07 +0200

    Remove old unused Docker files
    Signed-off-by: Nikola Forró <nforro@redhat.com>

--- ci/rpm-py-installer-docker/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-docker build -t rpm-py-installer-docker .
-docker tag rpm-py-installer-docker \
-    docker.io/junaruga/rpm-py-installer-docker:26
-docker push docker.io/junaruga/rpm-py-installer-docker:26
--- ci/rpm-py-installer-docker/entry.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo 'Using system Pythons and ohter Pythons installed by pyenv.'
-echo 'List up by "pyenv versions".'
-pyenv versions
-
-exec "${@}"
